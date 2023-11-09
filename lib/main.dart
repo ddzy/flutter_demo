15612901635aba48508c5cd09a22e79acece665f
ddzy <1766083035@qq.com>
2023-11-09 16:44:25 +0800

    feat(Counter): 实现基本的计数器

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,60 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     title: 'test',
-    home: Container(
-      child: const SafeArea(child: MyApp(), bottom: false),
-      color: Colors.blue[500],
-    ),
+    home: HomePage(),
   ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Material(
-      child: Column(
-        children: [
-          MyAppBar(
-              title: Text(
-            '测试标题',
-            style: TextStyle(
-                color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
-          )),
-          Expanded(child: Center(child: Text('测试内容')))
-        ],
-      ),
-    );
+  State<StatefulWidget> createState() {
+    return _HomePageState();
   }
 }
 
-class MyAppBar extends StatelessWidget {
-  final Widget title;
+class _HomePageState extends State<HomePage> {
+  _HomePageState();
 
-  const MyAppBar({required this.title, super.key});
+  int _count = 0;
+
+  void _increment() {
+    setState(() {
+      _count += 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 56,
-      padding: const EdgeInsets.symmetric(horizontal: 8),
-      decoration: BoxDecoration(color: Colors.blue[500]),
-      child: Row(
-        children: [
-          const IconButton(
-            onPressed: null,
-            icon: Icon(Icons.menu),
-            tooltip: '菜单',
-          ),
-          Expanded(child: title),
-          const IconButton(
+    return Scaffold(
+      appBar: AppBar(
+        leading: const IconButton(
+          onPressed: null,
+          icon: Icon(Icons.menu),
+          tooltip: 'Menu',
+        ),
+        title: const Text('Counter'),
+        actions: const [
+          IconButton(
             onPressed: null,
             icon: Icon(Icons.search),
-            tooltip: '搜索',
-          ),
+            tooltip: 'Search',
+          )
         ],
+      ),
+      body: Center(
+        child: Text(
+          'Count: $_count',
+          style: const TextStyle(fontSize: 20),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _increment,
+        tooltip: 'Add',
+        child: const Icon(Icons.add),
       ),
     );
   }

b9f2186989417ffa03f81fc29d5e485629d5f1e7
ddzy <1766083035@qq.com>
2023-12-01 10:15:05 +0800

    feat(Interval, AnimatedBuilder, Duration): 完善错乱的菜单动画

--- lib/menu_stagger_animation.dart
+++ lib/menu_stagger_animation.dart
@@ -1,3 +1,6 @@
+import 'dart:developer' as dev;
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class MenuStaggerAnimation extends StatefulWidget {
@@ -53,6 +56,12 @@ class _MenuStaggerAnimationState extends State<MenuStaggerAnimation>
   }
 
   @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
@@ -95,12 +104,53 @@ class MenuStagger extends StatefulWidget {
 
 class _MenuStaggerState extends State<MenuStagger>
     with SingleTickerProviderStateMixin {
+  List<String> textList = [
+    "Declarative style",
+    "Premade widgets",
+    "Stateful hot reload",
+    "Native performance",
+    "Great community",
+  ];
+
   late AnimationController controller;
+
+  /// 动画初始的延迟时间
+  late final Duration initialDelayTime;
+
+  /// 列表项运动的持续时间
+  late final Duration itemSlideTime;
+
+  /// 列表项运动前的延迟时间
+  late final Duration itemDelayTime;
+
+  /// 按钮运动的持续时间
+  late final Duration buttonTime;
+
+  /// 按钮运动前的延迟时间
+  late final Duration buttonDelayTime;
+
+  /// 动画持续总时长
+  late final Duration durationTime;
 
   @override
   void initState() {
     super.initState();
-    controller = AnimationController(vsync: this);
+
+    initialDelayTime = const Duration(milliseconds: 150);
+    itemSlideTime = const Duration(milliseconds: 150);
+    itemDelayTime = const Duration(milliseconds: 50);
+    buttonTime = const Duration(milliseconds: 300);
+    buttonDelayTime = const Duration(milliseconds: 150);
+    durationTime = initialDelayTime +
+        itemSlideTime +
+        itemDelayTime * (textList.length - 1) +
+        buttonDelayTime +
+        buttonTime;
+
+    controller = AnimationController(
+        vsync: this,
+        duration: Duration(milliseconds: durationTime.inMilliseconds))
+      ..forward();
   }
 
   /// logo
@@ -116,53 +166,85 @@ class _MenuStaggerState extends State<MenuStagger>
     );
   }
 
+  List<Widget> buildList() {
+    return textList
+        .asMap()
+        .map((dynamic key, value) {
+          // 计算每个列表项的动画曲线（即延长时间）
+          var tween = Tween(
+            begin: const Offset(1, 0),
+            end: Offset.zero,
+          );
+          // 每一个列表项的动画开始时间（在总的动画时长范围内，即 0 - 1）
+          var startTime = (initialDelayTime.inMilliseconds +
+              key * itemDelayTime.inMilliseconds);
+          // 每一个列表项的动画结束时间（在总的动画时长范围内，即 0 - 1）
+          var endTime = (startTime + itemSlideTime.inMilliseconds);
+          var animation = tween
+              .chain(CurveTween(
+                  curve: Interval(startTime / durationTime.inMilliseconds,
+                      endTime / durationTime.inMilliseconds)))
+              .animate(controller);
+
+          return MapEntry(
+              key,
+              AnimatedBuilder(
+                  animation: animation,
+                  builder: (context, child) {
+                    return SlideTransition(
+                      position: animation,
+                      child: ListTile(
+                        title: Text(value),
+                        subtitle: Text('${controller.value}'),
+                        contentPadding:
+                            const EdgeInsets.fromLTRB(36, 12, 0, 12),
+                        titleTextStyle: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            fontSize: 24),
+                      ),
+                    );
+                  }));
+        })
+        .values
+        .toList();
+  }
+
+  Widget buildButton() {
+    return Expanded(
+        child: Align(
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 12),
+        child: ElevatedButton(
+            onPressed: () {},
+            child: const Text("Get started"),
+            style: ButtonStyle(
+              shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(100))),
+              minimumSize: const MaterialStatePropertyAll(Size.fromHeight(50)),
+              textStyle: const MaterialStatePropertyAll(
+                TextStyle(fontSize: 20),
+              ),
+            )),
+      ),
+      alignment: const Alignment(0, 0.6),
+    ));
+  }
+
   /// menu-item
   Widget buildContent(BuildContext context) {
-    List<String> textList = [
-      "Declarative style",
-      "Premade widgets",
-      "Stateful hot reload",
-      "Native performance",
-      "Great community",
-    ];
-
     return Column(
       children: [
-        ...textList
-            .asMap()
-            .map((i, e) => MapEntry(
-                i,
-                ListTile(
-                  title: Text(e),
-                  contentPadding: const EdgeInsets.fromLTRB(36, 12, 0, 12),
-                  titleTextStyle: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                      fontSize: 24),
-                )))
-            .values
-            .toList(),
-        Expanded(
-            child: Align(
-          child: Container(
-            margin: const EdgeInsets.symmetric(horizontal: 12),
-            child: ElevatedButton(
-                onPressed: () {},
-                child: const Text("Get started"),
-                style: ButtonStyle(
-                  shape: MaterialStatePropertyAll(RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(100))),
-                  minimumSize:
-                      const MaterialStatePropertyAll(Size.fromHeight(50)),
-                  textStyle: const MaterialStatePropertyAll(
-                    TextStyle(fontSize: 20),
-                  ),
-                )),
-          ),
-          alignment: const Alignment(0, 0.6),
-        ))
+        ...buildList(),
+        buildButton(),
       ],
     );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 
   @override

cbc12823b5939ad272fb19cfa4e288ca3c6cc3a0
ddzy <1766083035@qq.com>
2023-12-06 15:11:06 +0800

    feat(FadeTransiton, SlideTransition, AnimatedSwitcher): 计数器显隐切换动画

--- lib/switcher_animation.dart
+++ lib/switcher_animation.dart
@@ -20,18 +20,29 @@ class _SwitcherAnimationState extends State<SwitcherAnimation> {
       body: Center(
         child: AnimatedSwitcher(
           duration: const Duration(milliseconds: 300),
+          reverseDuration: const Duration(milliseconds: 300),
           child: Text(
             "$num",
             style: const TextStyle(
               fontSize: 40,
               fontWeight: FontWeight.bold,
             ),
+            // Key 是必须的，不设置 key 的话，动画效果无法生效
             key: ValueKey(num),
           ),
           transitionBuilder: (child, animation) {
-            return FadeTransition(
-              opacity: animation,
-              child: child,
+            var tween = Tween<Offset>(
+              begin: const Offset(0, -1),
+              end: Offset.zero,
+            ).chain(CurveTween(curve: Curves.linear));
+            var newAnimation = tween.animate(animation);
+
+            return SlideTransition(
+              position: newAnimation,
+              child: FadeTransition(
+                opacity: animation,
+                child: child,
+              ),
             );
           },
         ),
